412886ab78b8e182f6b612171ea4d2b8da0ef891
Oliver Esser <oliver.esser@actano.de>
2017-11-28 15:05:30 +0100

    chore: move data.js into build directory

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@ set -o pipefail
 export EMSCRIPTEN=${HOME}/projects/emscripten/emsdk-portable/emscripten/1.37.22
 OXYGINE=${HOME}/projects/oxygine/oxygine-framework
 
-python ${OXYGINE}/tools/others/embed_folder_js.py -s ./data
 mkdir -p build
 cd build
+python ${OXYGINE}/tools/others/embed_folder_js.py -s ../data
 cmake -DCMAKE_TOOLCHAIN_FILE="${EMSCRIPTEN}/cmake/Modules/Platform/Emscripten.cmake" ..
 make
 cd ..
