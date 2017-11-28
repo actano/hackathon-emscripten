4a4c828f7899f342eafb57562b39103417a33134
Christoph Vitz <c.vitz@itsonix.eu>
2017-11-28 17:03:35 +0100

    Add derived class of draggable

--- src/example.cpp
+++ src/example.cpp
@@ -13,17 +13,23 @@ class MainActor;
 typedef oxygine::intrusive_ptr<MainActor> spMainActor;
 typedef oxygine::intrusive_ptr<Draggable> spDraggable;
 
+class CollidingDraggable : public Draggable
+{
+//    protected:
+//    void onMove(const Vector2& position) override {
+//    }
+};
+
 class MainActor: public Actor
 {
 public:
-    Draggable _draggable;
+    CollidingDraggable _draggable;
 
-    MainActor()
+    MainActor(Vector2 initialPosition)
     {
         spSprite button = new Sprite();
         button->setResAnim(gameResources.getResAnim("button"));
-        Vector2 pos = getStage()->getSize() / 2 - button->getSize() / 2;
-        button->setPosition(pos);
+        button->setPosition(initialPosition);
         addChild(button);
     }
 
@@ -38,9 +44,12 @@ void example_init()
 {
     gameResources.loadXML("res.xml");
 
-    spMainActor actor = new MainActor;
-    getStage()->addChild(actor);
-    actor->foo();
+    for (int i=0; i<3; i++) {
+        Vector2 pos = Vector2(100*i, 0);
+        spMainActor actor = new MainActor(pos);
+        getStage()->addChild(actor);
+        actor->foo();
+    }
 }
 
 void example_update()
